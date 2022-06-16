300302a94b714d49815fc78df47c434fbb5ba3f6
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:27:55 -0300

    Add files via upload

---
+++ 68-CargarNombres y Mostrar/PruebaParaPractica/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PruebaParaPractica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorLeft = 30;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Ingrese la cantidad de nombres:");
+            int CantidadDeNombre = Convert.ToInt32(Console.ReadLine());
+
+            string[] NombresIngresados = new string[CantidadDeNombre];
+
+            Console.WriteLine("Ahora ingrese los nombres:");
+
+            for (int i=0; i < CantidadDeNombre; i++)
+            {
+
+                NombresIngresados  [i]= Console.ReadLine();
+                
+            }
+            Console.Clear();
+
+            Console.CursorLeft = 30;
+            Console.WriteLine("Los nombres ingresados son:");
+            for (int i = 0; i < CantidadDeNombre; i++)
+            {
+                
+                Console.WriteLine(NombresIngresados[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
